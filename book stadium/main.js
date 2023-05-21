4b1b5735755f01c10105c85e410fc7a4e93781fc
Prisha Bhatia <72916732+PianoPianist@users.noreply.github.com>
2023-05-21 20:43:25 +0530

    Add files via upload

--- book stadium/main.js
+++ book stadium/main.js
@@ -2,5 +2,6 @@ const arr1 = document.querySelectorAll("a")
 
 for (var i = 0;i < arr1.length;i++){
 arr1[i].addEventListener("click", function(){
-    document.querySelector(".textdrop").text="Wankhde Stadium"
+    console.log("yes")
+    document.querySelector(".textdrop").textContent="Wankhde Stadium"
 })}
